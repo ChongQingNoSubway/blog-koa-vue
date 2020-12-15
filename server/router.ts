b2103745b3ae7594bdae4b88775f8a98da60c192
ChongQingNoSubway <zhuwenhiu@gmail.com>
2020-12-15 08:39:19 -0700

    interfacemodify

--- server/router.ts
+++ server/router.ts
@@ -1,6 +1,5 @@
 const koaRouter = require('koa-router')();
 const UserController = require('./controllers/UserController.ts');
-const SortController = require('./controllers/SortController.ts');
 const BlogController = require('./controllers/BlogController.ts');
 const FollowController = require('./controllers/FollowController.ts');
 const CommentController = require('./controllers/CommentController.ts');
@@ -31,21 +30,12 @@ module.exports = (app) => {
 
     // 2.博客资源RestFul API
     // 获取所有博客类型
-    koaRouter.get('/blogs/sort', SortController.getAllSorts);
     // 新增博客
     koaRouter.post('/blogs/new', BlogController.addBlog);
     // 更新博客
     koaRouter.put('/blogs/update', BlogController.updateBlog);
     // 获取所有博客
     koaRouter.get('/blogs/list', BlogController.getBlogs);
-    // 根据blog_id查询博客
-    koaRouter.get('/blogs/id/:blog_id', BlogController.getBlogById);
-    // 根据email查询博客
-    koaRouter.get('/blogs/email/:email', BlogController.getBlogsByEmail);
-    // 搜索博客
-    koaRouter.get('/blogs/search/:keyword', BlogController.searchBlog);
-    // 根据博客类型搜索博客
-    koaRouter.get('/blogs/sort/:sort_id', BlogController.getBlogBySortId);
     // 2.1博客评论
     // 添加评论
     koaRouter.post('/blogs/comment', CommentController.addComment);
--- server/services/SortService.ts
+++
@@ -1,8 +0,0 @@
-const SortModel = require('../models/SortModel.ts');
-
-module.exports = {
-    // 查询所有类型
-    findAll: async () => {
-        return await SortModel.findAll();
-    }
-}
